1b0421830f4bce96c94721c38381058131c7a636
Gabriel Vieira <gabrielcnt96@gmail.com>
2026-03-10 22:27:37 -0300

    feat: taskS5-T5.1 criar utilitario de geração de token

---
+++ app/core/jwt_token.py
@@ -0,0 +1,40 @@
+from datetime import datetime, timedelta
+from jose import jwt
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
+
+def create_access_token(data: dict) -> str:
+    to_encode = data.copy()
+
+    expire = datetime.now() + timedelta(
+        minutes=ACCESS_TOKEN_EXPIRE_MINUTES
+    )
+
+    to_encode.update({"exp": expire})
+
+    encode_jwt = jwt.encode(
+        to_encode,
+        SECRET_KEY,
+        algorithm=ALGORITHM
+    )
+
+    return encode_jwt
+
+
+def decode_token(token: str):
+
+    payload = jwt.decode(
+        token,
+        SECRET_KEY,
+        algorithms=ALGORITHM
+    )
+
+    user_id = payload.get("sub")
+
+    return user_id
