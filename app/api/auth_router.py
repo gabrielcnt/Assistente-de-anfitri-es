084d14cd55374d1f13ab7606cdb65c14107ec9bd
Gabriel Vieira <gabrielcnt96@gmail.com>
2026-03-11 21:27:36 -0300

    fix: tratamento de erro adicionado na rota /login

--- app/api/auth_router.py
+++ app/api/auth_router.py
@@ -1,9 +1,9 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 from app.core.config import get_db
 from app.schemas.user_schema import LoginRequest
-from app.services.auth_service import AuthService
+from app.services.auth_service import AuthService, CredencialInvalida
 from app.repositories.user_repo import UserRepository
 
 router = APIRouter(
@@ -16,11 +16,13 @@ def login(
     data: LoginRequest,
     db: Session = Depends(get_db)
 ):
-    
-    user_repository = UserRepository(db)
-    auth_service = AuthService(user_repository)
-    
-    return auth_service.login(
-        email=data.email,
-        password=data.password
-    )
+    try:
+        user_repository = UserRepository(db)
+        auth_service = AuthService(user_repository)
+        
+        return auth_service.login(
+            email=data.email,
+            password=data.password
+        )
+    except CredencialInvalida as e:
+        raise HTTPException(status_code=401, detail=str(e))
