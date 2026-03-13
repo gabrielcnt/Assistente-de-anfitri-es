6a394c02bedae4c9e8a70d9a651864e3babe64aa
Gabriel Vieira <gabrielcnt96@gmail.com>
2026-03-13 20:40:36 -0300

    feat: taskS7-T4 criar repository de embedding

---
+++ app/repositories/conhecimento_embedding_repo.py
@@ -0,0 +1,37 @@
+import json
+from sqlalchemy.orm import Session
+from app.models.conhecimento_embedding import ConhecimentoEmbedding
+
+
+class ConhecimentoEmbeddingRepository:
+
+    def __init__(self, db: Session):
+        self.db = db
+
+    
+    def criar(self, entidade, entidade_id, conteudo, vetor):
+
+        embedding_json = json.dumps(vetor)
+
+        registro = ConhecimentoEmbedding(
+            entidade=entidade,
+            entidade_id=entidade_id,
+            conteudo=conteudo,
+            embedding=embedding_json
+        )
+
+        self.db.add(registro)
+        self.db.commit()
+        self.db.refresh(registro)
+        
+        return registro
+    
+
+    def listar_por_entidade(self, entidade, entidade_id):
+        return (
+            self.db.query(ConhecimentoEmbedding).filter(
+                ConhecimentoEmbedding.entidade == entidade,
+                ConhecimentoEmbedding.entidade_id == entidade_id
+            )
+            .all()
+        )
