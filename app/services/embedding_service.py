ba66801093a9d869b27b1dba39fd201ea127fafd
Gabriel Vieira <gabrielcnt96@gmail.com>
2026-03-13 19:41:14 -0300

    feat: taskS7-T2 criar serviço de geração de embedding

---
+++ app/services/embedding_service.py
@@ -0,0 +1,12 @@
+from app.core.openai_client import client
+
+def gerar_embedding(texto: str):
+    
+    response = client.embeddings.create(
+        model="text-embedding-3-small",
+        input=texto
+    )
+
+    vetor = response.data[0].embedding
+    
+    return vetor
